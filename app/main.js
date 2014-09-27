e53f812e910e1dc20faa5221a1aa20123c98dea8
unknown <sadikr@ULTP_337.synerzipune.local>
2014-09-27 12:44:10 +0530

    Chart added.

--- app/main.js
+++ app/main.js
@@ -7,7 +7,8 @@ requirejs.config({
 
 	paths : {
 		jquery : "components/jquery/dist/jquery.min",
-		d3 : "components/d3/d3.min"
+		d3 : "components/d3/d3.min",
+		playChart: "js/playChart"
 	},
 
 	shim : {
@@ -15,14 +16,22 @@ requirejs.config({
 			exports : 'd3'
 		}
 	},
-
 });
 /**
  * [description]
- * @param  {[type]} $
+ * @param  {[type]} $ jQuery Object
  * @param  {[type]} d
  * @return {[type]}
  */
-require(["jquery","d3","playChart"],function($,d) {
+require(["playChart"],function(Chart) {
 	// body...
+	Chart.initialize({
+		controllersEl : $("#button_wrapper"),
+		chartEl : $("#chart_wrapper"),
+		play : true,
+		pause: false,
+		stop: false,
+		fastForward: false,
+		rewind: false
+	});
 });
